a814d2e5c4daed8a8d6a699d63fad0ea34680eca
lupinge <849836178@qq.com>
2016-03-04 21:11:18 +0800

    css3

--- index/index.js
+++ index/index.js
@@ -1,3 +1,13 @@
 $(function(){
     window.console.log('site ok');
+
+    $('.avatar').on('animationend', function() {
+   		$(this).removeClass('avatarFadeIn').removeClass('avatarScale3d');
+    }).on('webkitAnimationEnd', function () {
+        $(this).removeClass('avatarFadeIn').removeClass('avatarScale3d');
+    });
+
+    $('.avatar').click(function(){
+    	$(this).addClass('avatarScale3d');
+    });
 });
